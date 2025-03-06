24ee65b4e8ca94b2d8434cc30b8e1c86a70509fe
Tim <timletelgte11@gmail.com>
2025-03-06 22:05:14 +0100

    I gave up

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,29 +1,36 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import html2canvas from "html2canvas";
+import { SidebarContent, SidebarGroup, SidebarHeader } from "@/components/ui/sidebar";
+import { Sidebar } from "lucide-react";
 import React, { useEffect, useState } from "react";
-import Tesseract from "tesseract.js";
 
 export default function Page() {
   const [dataCode, setDataCode] = useState("");
-  useEffect(() => {
-    console.log(dataCode);
-  }, [dataCode]);
 
   return (
     <>
       <title>Game</title>
-      <div className="w-screen h-screen flex flex-col items-center">
-        <Button className="w-fit m-3" onClick={() => console.log("clicked")}>
-          Scan
-        </Button>
-        <iframe
-          src="https://scratch.mit.edu/projects/1142499853/embed"
-          width="485"
-          height="402"
-          className="w-[90%] h-[90%] border-2 border-black bg-[rgb(255,0,0)]"
-        ></iframe>
+      <div className="w-screen h-screen">
+        <Sidebar>
+          <SidebarHeader />
+          <SidebarContent>
+            <SidebarGroup>
+              Hekk
+            </SidebarGroup>
+          </SidebarContent>
+        </Sidebar>
+        <div className="w-screen h-screen flex flex-col items-center">
+          <Button className="w-fit m-3" onClick={() => console.log("clicked")}>
+            Scan
+          </Button>
+          <iframe
+            src="https://scratch.mit.edu/projects/1142499853/embed"
+            width="485"
+            height="402"
+            className="h-[90%] w-[54%] border-2 border-black bg-[rgb(255,0,0)]"
+          ></iframe>
+        </div>
       </div>
     </>
   );
