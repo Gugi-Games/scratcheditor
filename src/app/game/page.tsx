3a9c145e990636c35b92acab7a448c7d9187fa40
unknown <timletelgte11@gmail.com>
2025-03-06 12:20:02 +0100

    removed unnessecarry code

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -7,37 +7,6 @@ import Tesseract from "tesseract.js";
 
 export default function Page() {
   const [dataCode, setDataCode] = useState("");
-
-  const displayCanvasInNewWindow = (canvas) => {
-    const newWindow = window.open("", "Canvas Preview", "width=800,height=600");
-    if (newWindow) {
-      newWindow.document.body.appendChild(canvas);
-    }
-  };
-
-  async function handleClick() {
-    const iframe = document.querySelector("iframe");
-    if (!iframe) return;
-    try {
-      // Capture the iframe directly
-      const canvas = await html2canvas(iframe, {
-        useCORS: true,
-        logging: false,
-      });
-
-      displayCanvasInNewWindow(canvas);
-
-      // Run OCR on the canvas
-      const result = await Tesseract.recognize(canvas, "eng", {
-        logger: (m) => console.log(m),
-      });
-
-      setDataCode(result.data.text);
-    } catch (error) {
-      console.error("Error:", error);
-    }
-  }
-
   useEffect(() => {
     console.log(dataCode);
   }, [dataCode]);
@@ -46,7 +15,7 @@ export default function Page() {
     <>
       <title>Game</title>
       <div className="w-screen h-screen flex flex-col items-center">
-        <Button className="w-fit m-3" onClick={handleClick}>
+        <Button className="w-fit m-3" onClick={() => console.log("clicked")}>
           Scan
         </Button>
         <iframe
